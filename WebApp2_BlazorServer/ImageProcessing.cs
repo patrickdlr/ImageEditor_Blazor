9cac170ccc952b3472fac5c1457659857c3b55e1
Patrick De La Rosa <patrickdelarosa42@gmail.com>
2021-05-27 22:23:35 -0700

    Add project files.

---
+++ WebApp2_BlazorServer/ImageProcessing.cs
@@ -0,0 +1,53 @@
+﻿using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats;
+using System;
+
+using System.IO;
+using System.Diagnostics;
+using SixLabors.ImageSharp.Processing;
+using SixLabors.ImageSharp.PixelFormats; //< rgba32 >
+
+namespace BlazorServerApp1
+{
+    public class ImageProcessing_CS : IDisposable
+    {
+        private Image originalImage;
+        private Image editImage;
+        private IImageFormat IIF1;
+
+        public ImageProcessing_CS(Image image, IImageFormat imageFormat)
+        {
+            this.IIF1 = imageFormat;
+            this.originalImage = image;
+            this.editImage = image;
+        }
+
+        public string getBase64String_function()
+        {
+            return editImage.ToBase64String(IIF1);
+        }
+
+        public Image GetImage()
+        {
+            return this.editImage;
+        }
+
+        public void UpdateImage(Image image)
+        {
+            this.editImage = image;
+        }
+
+        public void ResetImage_function()
+        {
+            editImage = originalImage;
+        }
+
+        public void Dispose()
+        {
+            originalImage.Dispose();
+            editImage.Dispose();
+        }
+    }
+
+
+}
