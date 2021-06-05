f5faf6ab8f65d3e66b5d5db3cfca048a1c9f31d0
Patrick De La Rosa <patrickdelarosa42@gmail.com>
2021-06-04 21:44:07 -0700

    c

--- WebApp2_BlazorServer/Program.cs
+++ WebApp2_BlazorServer/Program.cs
@@ -1,10 +1,16 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using System;
+using SixLabors.ImageSharp;
+using System.IO;
 
 namespace WebApp2_BlazorServer {
     public class Program {
         public static void Main(string[] args) {
             CreateHostBuilder(args).Build().Run();
+
+
+            
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
